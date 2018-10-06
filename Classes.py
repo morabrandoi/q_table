257e7779f25d7b8d2fb9bc09cd1a7ae23d23a1dd
morabrandoi <morabrandoi@gmail.com>
2018-10-06 18:12:17 -0400

    first commit

--- Classes.py
+++ Classes.py
@@ -1,6 +1,11 @@
 import random
 import pygame
 
+# fix q-dict structure so direction correspondss to an index of the list as the value
+# fix decesion process
+# create update q_dict method
+
+
 
 class Board:
     def __init__(self, dimension, display_size):
@@ -14,6 +19,7 @@ class Board:
         self.bot = None
         self.finish = None
         self.pellets = None
+        self.pair_history = []
         self.q_dict = {}
 
     def set_actors(self, actors):
@@ -34,7 +40,6 @@ class Board:
 
     def confirm_move(self, actor):
         dir = actor.decide_move()
-
         if dir == "right" and (actor.x + self.tile_size <= self.tile_location_coord[-1]):
             actor.move(dir)
             return dir
@@ -50,18 +55,21 @@ class Board:
         else:
             return "stay"
 
-
-
     def is_collided(self, actor1, actor2):
         if actor1.get_location() == actor2.get_location():
             return True
         else:
             return False
 
-    def reset_board(self):
+    def get_state(self):
+        state = []
         for actor in self.actors:
-            actor.x = actor.start_x
-            actor.y = actor.start_y
+            state.append(actor.get_location)
+        return tuple(state)
+
+    def add_state_action_pair(self, action):
+        state = self.get_state()
+        self.pair_history.append((state, action))
 
     def display_board(self, actors):
         self.game_display.fill((255,255,255))
@@ -70,23 +78,31 @@ class Board:
         pygame.display.update()
         self.clock.tick(60)
 
+    def update_dict(self, reward):
+        for
+
+    def reset_board(self):
+        for actor in self.actors:
+            actor.x = actor.start_x
+            actor.y = actor.start_y
+        self.pair_history = []
+
+
     def run_episode(self):
 
         game_exit = False
+        total_reward = 0
 
         while not game_exit:
-            total_reward = 0
-
-
             total_reward -= 1
-
 
             if self.is_collided(self.bot, self.finish):
                 print("true")
                 game_exit = True
                 total_reward += 1000
 
-            self.confirm_move(self.bot)
+            action = self.confirm_move(self.bot)
+            self.add_state_action_pair(action)
 
             self.display_board(self.actors)
 
@@ -94,15 +110,7 @@ class Board:
                 if event.type == pygame.QUIT:
                     game_exit = True
 
-
-
-
-
-
-
-
-
-
+        self.update_dict(total_reward)
 
 class Generic_Block:
     def __init__(self, start_loc=(-1,-1), color=(255,255,255)):
@@ -149,21 +157,12 @@ class Bot(Generic_Block):
         super().__init__(start_loc, (0,0,255))
 
 
-    # def decide_move(self, q_dict=None, state=None):
-    #     # # state is location
-    #     # # value is reward for action in order of L, U, R, D
-    #     #
-    #     # state = tuple(state)
-    #     # if state not in q_dict.keys():
-    #     #     q_dict[state] = [0,0,0,0]
-    #     # indices = [i for i, x in enumerate(q_dict[state]) if x == max(q_dict[state])]
-    #     # if len(indices) > 1:
-    #     #     index = random.choice(indices)
-    #     # else:
-    #     #     index = indices[0]
-    #     #
-    #     # return (["left", "up", "right", "down"][index], q_dict)
-    #     return random.choice(["left", "up", "right", "down"])
+    def decide_move(self, q_dict=None, state=None):
+        state = tuple(state)
+        if state not in q_dict.keys():
+            q_dict[state] = [0,0,0,0]
+        else:
+            pass
 
 
 class Pellet(Generic_Block):
